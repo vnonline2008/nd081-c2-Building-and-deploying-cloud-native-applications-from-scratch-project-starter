3df52606d4e284ec4a3b9fc8659d273a1d7400ba
vnonline2008 <vnonline2008@gmail.com>
2022-09-14 03:09:58 +0700

    add docker file and update fix error event hub

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -4,12 +4,10 @@ import azure.functions as func
 
 
 def main(event: func.EventGridEvent):
-
-	logging.info('Function triggered to process a message: ', event.get_body())
+    logging.info('Function triggered to process a message: ', event.get_body())
     logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
     logging.info('  SequenceNumber =', event.sequence_number)
     logging.info('  Offset =', event.offset)
-
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -20,6 +18,3 @@ def main(event: func.EventGridEvent):
 
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
-
-
-
